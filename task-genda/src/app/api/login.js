55071e593596359e322265fb3c07358a750a6960
Choll Sak <koonnew8688.cookie@gmail.com>
2024-10-11 00:24:43 +0700

    ds

--- task-genda/src/app/api/login.js
+++
@@ -1,8 +0,0 @@
-import { login } from '../../controllers/authController';
-
-export default function handler(req, res) {
-  if (req.method === 'POST') {
-    return login(req, res);
-  }
-  res.status(405).json({ message: 'Method Not Allowed' });
-}
